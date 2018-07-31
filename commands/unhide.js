4297240ed3c635520f37602ddb64456d55154456
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-07-31 13:51:06 +0200

    Update unhide.js

--- commands/unhide.js
+++ commands/unhide.js
@@ -3,7 +3,6 @@ const config = require(`../config.json`)
 module.exports.run = async (client, message, args) => {
   if(!message.member.hasPermission("MANAGE_CHANNELS"))
   return message.reply("Nie masz uprawnień")
-    if(util.val() == false) return message.reply('Komenda jest wyłączona');
     let everyone = message.guild.roles.find(`name`, "@everyone");
       await message.channel.overwritePermissions(everyone, {
                     SEND_MESSAGES: true,
