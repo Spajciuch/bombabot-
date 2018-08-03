ea2ec647dd368738f15fd61a5ff71a11efa1a865
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-08-03 12:56:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -26,11 +26,14 @@ client.on('ready', () => {
 		let online = ytspeak.members.filter(member => member.user.presence.status !== 'offline');
 	var hr = new Date().getHours() +2
 	if(hr == 25) hr = 1
+		var high = ''
+		if(high<online) high = online
   client.channels.get("470869924647141397").edit({name: `Osób online: ${online.size-ytspeak.members.filter(m => m.user.bot).size}`});
   client.channels.get("470869840027189248").edit({name: `Liczba Członków: ${ytspeak.memberCount}`});
   client.channels.get("470869709525614592").edit({name: `Liczba botów: ${ytspeak.members.filter(m => m.user.bot).size}`})
   client.channels.get("473410352013508610").edit({name: `Data: ${moment.utc(new Date()).format('DD.MM.YYYY')}`})
   client.channels.get("473410461669392385").edit({name: `Godzina: ${hr}:${moment.utc(new Date()).format('mm:ss')}`})
+  client.channels.get("474892701213523968").edit({name:`Rekord Online: ${high}`})
   },1000)
   console.log(`Zalogowano jako ${client.user.username}`);
 });
